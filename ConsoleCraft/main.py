16eac13fef841495717b574d9dc7c994518bdc05
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-07-01 12:49:30 +1200

    added entities and input

--- ConsoleCraft/main.py
+++ ConsoleCraft/main.py
@@ -7,15 +7,40 @@ from entity import Entity
 
 
 # Functions
-def main_loop(world):
-    entities = []
+def main_loop(world : World, player : Entity):
+    entities = [player]
     while True:
-        render.print_map(world, Entity())
+        render.print_map(world, entities, player)
+        render.clear_line(22)
+        user_input = input(">")
+        player, world = handle_inputs(user_input, player, world)
+
+
+def handle_inputs(user_input : str, player : Entity, world : World):
+    if user_input == "w":
+        if player.y > 0 and not world.get_tile_data(player.x, player.y - 1)["collides"]:
+            player.y -= 1
+    elif user_input == "a":
+        if player.x > 0 and not world.get_tile_data(player.x - 1, player.y)["collides"]:
+            player.x -= 1
+    elif user_input == "s":
+        if player.y < world.height - 1 and not world.get_tile_data(player.x, player.y + 1)["collides"]:
+            player.y += 1
+    elif user_input == "d":
+        if player.x < world.width - 1 and not world.get_tile_data(player.x + 1, player.y)["collides"]:
+            player.x += 1
+    elif user_input == "e":
+        world.set_tile(player.x, player.y, 1)
+    
+    return player, world
 
 
 # Runs setup for rendering
 print("Setting up Rendering")
 render.setup()
+print(render.set_colour("bright green"))
+print("This text should be in green.")
+print(render.set_colour("white"))
 
 
 # Loading
@@ -27,6 +52,15 @@ tileset_data = {}
 _dir = "Data/Tile Data.json"
 with open(_dir, "r") as f:
     tileset_data = json.load(f)
+
+
+# Loading Entities
+entity_data = {}
+_dir = "Data/Entities"
+for filename in os.listdir(_dir):
+    with open(os.path.join(_dir, filename), "r") as f:
+        data = json.load(f)
+        entity_data[data["name"]] = data
 
 
 # Loading Generation Steps
@@ -45,8 +79,12 @@ for filename in os.listdir(_dir):
 # Initializing World and Player
 print("Initializing World")
 world = World(10000, 10000, tileset_data, generation_steps)
+player = Entity(entity_data["human"])
+player.x = 5000
+while not world.get_tile_data(player.x, player.y + 1)["collides"]:
+    player.y += 1
 
 print("Running Main Loop")
-main_loop(world)
+main_loop(world, player)
 
-input("enter to exit...")
+input("Enter to exit...")
