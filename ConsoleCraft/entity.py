84a61a8428b4bd1336454042c295cb1c3f2ad9ae
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-07-01 12:49:43 +1200

    implemented

--- ConsoleCraft/entity.py
+++ ConsoleCraft/entity.py
@@ -3,3 +3,11 @@
 class Entity:
     x = 0
     y = 0
+    visible = False
+
+
+    def __init__(self, data : dict) -> None:
+        if data["visible"]:
+            self.visible = True
+            self.char = data["char"]
+            self.colour = data["colour"]
