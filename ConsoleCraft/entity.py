24bca18eafd21a0d2d759a4896a68dc4df6adba1
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-07-02 13:33:20 +1200

    started adding movement

--- ConsoleCraft/entity.py
+++ ConsoleCraft/entity.py
@@ -1,9 +1,10 @@
-
+from world import World
 
 class Entity:
     x = 0
     y = 0
     visible = False
+    moves = False
 
 
     def __init__(self, data : dict) -> None:
@@ -11,3 +12,24 @@ class Entity:
             self.visible = True
             self.char = data["char"]
             self.colour = data["colour"]
+        if data["moves"]:
+            self.moves = True
+            self.climbs =  True
+            self.falls = True
+    
+
+    def move(self, direction : str, amount : int, world : World):
+        velocity = [0, 0] #vector2d: x, y
+        
+        # Evaluate direction
+        if direction == "left":
+            velocity[0] = -1 * amount
+        elif direction == "right":
+            velocity[0] = 1 * amount
+        elif direction == "up":
+            velocity[1] = -1 * amount
+        elif direction == "down":
+            velocity[1] = 1 * amount
+        
+        if falls:
+            pass
