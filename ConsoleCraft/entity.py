4646f9eec73ead07a239809b4b5fb27c8db1f466
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-07-03 15:13:57 +1200

    finished adding movement

--- ConsoleCraft/entity.py
+++ ConsoleCraft/entity.py
@@ -17,24 +17,29 @@ class Entity:
             self.colour = data["colour"]
         if data["moves"]:
             self.moves = True
-            self.climbs =  True
-            self.falls = True
+            self.climbs =  data["climbs"]
+            self.falls = data["falls"]
     
 
     def move(self, direction : str, world : World) -> None:
         if direction == "left" or direction == "right":
             x = {"left": -1, "right": 1}[direction]
-            if not world.does_collide(self.x + x, y):
+            if not world.collides(self.x + x, self.y):
                 self.x += x
             elif self.climbs and self.y > 0:
-                if not world.does_collide(self.x + x, self.y - 1):
-                    self.x += x
-                    self.y -= 1
-        elif direction == "up":
-            pass
-        elif direction == "down":
-            pass
-    
+                if not world.collides(self.x, self.y - 1):
+                    if not world.collides(self.x + x, self.y - 1):
+                        self.x += x
+                        self.y -= 1
+        else:
+            y = {"up": -1, "down": 1}[direction]
+            if not world.collides(self.x, self.y + y):
+                self.y += y
+        
+        if self.falls:
+            while not world.collides(self.x, self.y + 1):
+                self.y += 1
+
 
     def tick(self, world : World) -> None:
         pass
